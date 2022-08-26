6bc485eb5dafe508f2e1dabc0f86ff06dd88a412
Chris Milbert <cmilbert>
2022-08-26 18:41:44 -0400

    Moving Timer to a submodule

--- src/stats.rs
+++ src/stats.rs
@@ -1,11 +1,16 @@
+mod timer;
+
+use timer::Timer;
+
 use crossbeam_channel::Receiver;
 use crossterm::{
     cursor, execute,
     style::{self, Color, PrintStyledContent, Stylize},
     terminal::{Clear, ClearType},
 };
+
 use std::io::{self, Result, Stderr, Write};
-use std::time::{Duration, Instant};
+use std::time::Instant;
 
 pub fn stats_loop(silent: bool, stats_rx: Receiver<usize>) -> Result<()> {
     let mut total_bytes = 0;
@@ -52,13 +57,6 @@ fn output_progress(stderr: &mut Stderr, bytes: usize, elapsed: String, rate: f64
     );
     let _ = stderr.flush();
 }
-struct Timer {
-    last_instant: Instant,
-    delta: Duration,
-    period: Duration,
-    countdown: Duration,
-    ready: bool,
-}
 
 trait TimeOutput {
     fn as_time(&self) -> String;
@@ -69,28 +67,5 @@ impl TimeOutput for u64 {
         let (hours, left) = (*self / 3600, *self % 3600);
         let (minutes, seconds) = (left / 60, left % 60);
         format!("{}:{:02}:{:02}", hours, minutes, seconds)
-    }
-}
-
-impl Timer {
-    fn new() -> Self {
-        let now = Instant::now();
-        Self {
-            last_instant: now,
-            delta: Duration::default(),
-            period: Duration::from_millis(1000),
-            countdown: Duration::default(),
-            ready: true,
-        }
-    }
-
-    fn update(&mut self) {
-        let now = Instant::now();
-        self.delta = now - self.last_instant;
-        self.last_instant = now;
-        self.countdown = self.countdown.checked_sub(self.delta).unwrap_or_else(|| {
-            self.ready = true;
-            self.period
-        });
     }
 }
